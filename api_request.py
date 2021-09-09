60f2255adc1951cf770da9f9bdb13e018ba14e99
nick <smz1680@gmail.com>
2021-09-09 08:20:03 -0700

    adding os.environ for heroku

--- api_request.py
+++ api_request.py
@@ -1,25 +1,28 @@
+import os
 from api_keys import wordsapi
 from api_keys import serpapi
 from serpapi import GoogleSearch
 import requests
 
+
+
 def get_english_def(vol):
     url = f"https://wordsapiv1.p.rapidapi.com/words/{vol}/definitions"
     headers = {
-        'x-rapidapi-key':wordsapi
+        'x-rapidapi-key':os.environ.get('WordsApi_Key',wordsapi)
     }
     response = requests.request("GET", url, headers=headers)
     data = response.json()
     return data["definitions"][0]
 
 def get_definition_image(search):
-  params = {
-  "q": search,
-  "tbm": "isch",
-  "ijn": "0",
-  "api_key": serpapi}
+    params = {
+    "q": search,
+    "tbm": "isch",
+    "ijn": "0",
+    "api_key": os.environ.get('GoogleSearch_Key',serpapi)}
 
-  search = GoogleSearch(params)
-  results = search.get_dict()
-  return results['images_results'][:20]
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    return results['images_results'][:20]
 
